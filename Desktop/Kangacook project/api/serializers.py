a80b7536d426172f951c28ea0d26d655db3ccb91
andrew <aihof757@gmail.com>
2024-10-21 13:53:24 -0400

    made folder adjustment

--- Desktop/Kangacook project/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ShortenedUrl
-
-class ShortenedUrlSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShortenedUrl
-        fields = '__all__'
--- Desktop/Kangacook project/src/index.js
+++
@@ -1,4 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
